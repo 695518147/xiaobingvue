233ac9caf3ae4e7d862b22d4c2f8a7fa2e902e7c
zhangpeiyu <695518147@qq.com>
2019-09-01 15:07:46 +0800

    优化

--- src/components/right/right.vue
+++ src/components/right/right.vue
@@ -5,10 +5,12 @@
                 <div class="col-md-12 column">
                     <div class="list-group">
 
-                        <div id="content2" class="list-group-item" v-html="order.orderDescription" v-if="order.split">
+                        <div id="content2" class="list-group-item" v-html="parse(order.orderDescription,'content2')"
+                             v-if="order.split">
                         </div>
                         <hr v-if="order.split">
-                        <div id="content1" class="list-group-item" v-html="order.orderTypeDescription">
+                        <div id="content1" class="list-group-item"
+                             v-html="parse(order.orderTypeDescription, 'content1')">
 
                         </div>
                     </div>
@@ -19,6 +21,8 @@
 </template>
 
 <script>
+    import $ from 'jQuery'
+
     export default {
         name: "right",
         computed: {
@@ -26,5 +30,26 @@
                 return this.$store.state.order;
             }
         },
+        methods: {
+            parse(str, parentId) {
+                var objE = document.createElement("div");
+                objE.innerHTML = str;
+                let $parent = $("#" + parentId)
+                let $dom = $(objE).children();
+                $dom.find("img").each((index, item) => {
+                    if ($(item).prop("width") < $parent.width()) {
+                        let width = $(item).prop("width");
+                        $(item).width(width)
+                    } else {
+                        let width = $parent.width();
+                        let height = $(item).prop('height') * ($parent.width() * 1.0 / $(item).prop("width"));
+                        $(item).width(width);
+                        $(item).height(height)
+                    }
+                });
+                console.log($dom.html());
+                return $(objE).html();
+            }
+        }
     };
 </script>
