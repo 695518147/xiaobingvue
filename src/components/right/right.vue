7d949940408e5ea2232c57905dd5c489cd57b25d
zhangpeiyu <695518147@qq.com>
2019-09-05 20:27:40 +0800

    去除cheerio（$）

--- src/components/right/right.vue
+++ src/components/right/right.vue
@@ -20,9 +20,6 @@
 </template>
 
 <script>
-    import cheerio from 'cheerio'
-
-
     export default {
         name: "right",
         computed: {
@@ -40,30 +37,35 @@
             if (!(this.$store.state.order.id == undefined)) {
                 if (this.$store.state.order.split) {
                     let dom_content2 = document.getElementById("content2")
-                    this.parse(dom_content2, this.$store.state.order.orderDescription)
+                    dom_content2.innerHTML = this.$store.state.order.orderDescription
+                    this.parse(dom_content2)
                 }
                 let dom_content1 = document.getElementById("content1")
-                this.parse(dom_content1, this.$store.state.order.orderTypeDescription)
+                dom_content1.innerHTML = this.$store.state.order.orderTypeDescription
+                this.parse(dom_content1)
             }
             let stop = new Date().getTime()
             console.log("stop - start", stop - start)
         },
         methods: {
-            parse(dom, description) {
+            parse(dom) {
                 let p_width = dom.clientWidth
-                const $ = cheerio.load(description)
-                $("img").each((index, node) => {
-                    if (node.tagName == "img") {
-                        let c_width = $(node).attr("width")
-                        if (c_width < p_width) {
-                            $("img").css("width", c_width + 'px')
-                        } else {
-                            let height = $(node).attr('height') * (p_width * 1.0 / c_width)
-                            $("img").css("height", height + 'px')
-                        }
+                console.log("p_width", p_width)
+                let nodeIterator = document.createNodeIterator(dom, NodeFilter.SHOW_ELEMENT, (node) => {
+                    return node.nodeName.toLowerCase() === 'img' ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
+                });
+                let currentNode;
+                while (currentNode = nodeIterator.nextNode()) {
+                    let c_width = currentNode.getAttribute("width")
+                    console.log("c_width", c_width)
+                    if (c_width < p_width) {
+                        currentNode.style.width = c_width + 'px'
+                    } else {
+                        let height = currentNode.getAttribute("height") * (p_width * 1.0 / c_width)
+                        console.log("height", height)
+                        currentNode.style.height = height + 'px'
                     }
-                })
-                dom.innerHTML = $('body').html()
+                }
             }
         }
     };
